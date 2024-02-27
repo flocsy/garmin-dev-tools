dc9ac22ea77e82029f74ab103ed5adcf16f4e977
Gavriel Fleischer <flocsy@gmail.com>
2024-02-27 14:06:58 +0200

    don't add lines to csv when there's no data

--- scripts/garmin-download-stats.sh
+++ scripts/garmin-download-stats.sh
@@ -49,25 +49,32 @@ fetch_stats() {
     fi
 
     # the new data is in the json:
-    JSON="/tmp/garmin-app-${APP_ID}.${DOMAIN}.json"
+    JSON="/tmp/garmin-app-${APP_ID}.${DOMAIN}.stats.json"
     curl -s "https://apps.garmin.${DOMAIN}/api/appsLibraryExternalServices/api/asw/apps/${APP_ID}" > "${JSON}"
     STATUS=$(jq -r '.status' < "${JSON}")
     if [[ x"$STATUS" == x"404" ]]; then
         # the old data was in the html:
-        HTML="/tmp/garmin-app-${APP_ID}.${DOMAIN}.html"
+        HTML="/tmp/garmin-app-${APP_ID}.${DOMAIN}.stats.html"
         curl -s "https://apps.garmin.${DOMAIN}/en-US/apps/${APP_ID}" > "${HTML}"
         DOWNLOADS=$(pup "span.stat-adds span:last-child text{}" < "${HTML}")
         RATING=$(pup ".app-detail span.stat-rating .rating attr{title}" < "${HTML}")
         REVIEWS=$(pup "span.stat-rating .badge text{}" < "${HTML}")
         EXT_VER=$(pup "ul.list-unstyled div li text{}" < "${HTML}" | sed -e 's#Version:*##' | sed -e 's#^ *##' | grep -v '^$')
         NEW_DATA="${DOWNLOADS},${RATING},${REVIEWS},,${EXT_VER}"
+        #   "latestInternalVersion": 11,
+        #   "latestExternalVersion": "1.3",
+        #   "firstApprovalDate": 1654272950000,
+        #   "lastApprovalDate": 1654272950000,
+        #   "creationDate": 1654249648000,
+        #   "releaseDate": 1707806778000,
+# TODO: read releaseDate and add the missing data for the last n lines
     else
         NEW_DATA=$(jq -r '(.downloadCount|tostring) + "," + (.averageRating|tostring) + "," + (.reviewCount|tostring) + "," + (.latestInternalVersion|tostring) + "," + .latestExternalVersion' < "${JSON}")
     fi
 
     LAST_DATA=$(tail -n 1 "${CSV}" | sed -e 's#^[^,]*,##')
     #echo "last: $LAST_DATA, current: ${DOWNLOADS},${RATING},${REVIEWS}" >> /tmp/debug
-    if [[ x"${LAST_DATA}" != x"${NEW_DATA}" ]] && [[ x"${NEW_DATA}" != x"${EMPTY_LINE}" ]]; then
+    if [[ x"${LAST_DATA}" != x"${NEW_DATA}" ]] && [[ x"${NEW_DATA}" != x"${EMPTY_LINE}" ]] && [[ x"${NEW_DATA}" != x"" ]]; then
         DATE=$(date +%Y-%m-%d)
         echo "${DATE},${NEW_DATA}" >> "${CSV}"
     fi
